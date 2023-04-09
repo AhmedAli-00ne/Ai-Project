6d21ee3a72b2b35d4e3aa536e46ed7189dbf6318
AhmedAli-00ne <84929946+AhmedAli-00ne@users.noreply.github.com>
2023-04-09 21:11:22 +0200

    added game while loop and quit condition

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pygame
 Width = 500
 Height = 500
 Window = pygame.display.set_mode((Width, Height))
+
+def main():
+    Game = True
+    while Game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Game = False
