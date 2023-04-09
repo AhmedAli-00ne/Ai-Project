3f7128154ca01d4acc90c89c4a98d038326aa157
AhmedAli-00ne <84929946+AhmedAli-00ne@users.noreply.github.com>
2023-04-09 21:17:57 +0200

    capped fps at 60 and added window caption

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pygame
 Width = 500
 Height = 500
 Window = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Ai Project")
 
 def main():
+    clock = pygame.time.Clock()
     Game = True
     while Game:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Game = False
+        pygame.display.update()
     pygame.quit()
 if __name__ == "__main__":
     main()
